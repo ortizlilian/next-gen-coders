89602ce8ed82ae1a224eedbe5505f29ddbf9adc6
Yogita Negi <ynegi6@gmail.com>
2023-03-15 14:26:12 +0000

    Added search icon and functionality for search box to handle userinput

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,22 @@
-import {Box, Flex, Button, Input} from '@chakra-ui/react';
+import { useState } from 'react';
+import {Box, Flex, Button, Input, InputGroup, InputRightElement, IconButton} from '@chakra-ui/react';
+import { SearchIcon } from '@chakra-ui/icons';
 import Image from 'next/image'
 
 
 const NavBar = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  
+  const handleSearch = (e) => {
+    
+    console.log("Searching for:", searchQuery);
+    // logic to be added to display search results here
+  };
+
+  const handleInputChnage = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
   return (
     <Flex bg="gray.100" justify="space-between" align="center" p="4" w="100%" as='nav' wrap='wrap'>
       <Box pr={4}>
@@ -14,8 +28,22 @@ const NavBar = () => {
         />
       </Box>
       <Box display="flex" alignItems="center">
-        <Input placeholder="Search..." size="lg" />
+        <InputGroup size="lg">
+          <InputRightElement pointerEvents="none">
+            <IconButton aria-label="search database" icon={<SearchIcon />} onClick={handleSearch}/>
+
+          </InputRightElement>
+           
+          <Input placeholder="Search..." value={searchQuery} onChange={handleInputChnage} />
+
+          
+        </InputGroup>
+       
+        
       </Box>
+      
+        
+     
       <Box  flex="0 0 auto" pl={[0,4]}>
         <Button>Write Post</Button>
       </Box>
