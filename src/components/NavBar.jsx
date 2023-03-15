dc3768cee5f58957bc0e0bc69a09ee7c7a65056c
Yogita Negi <ynegi6@gmail.com>
2023-03-15 13:55:24 +0000

    Adjusted the size of logo , search box and write post,made responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,28 +4,23 @@ import Image from 'next/image'
 
 const NavBar = () => {
   return (
-    <Flex bg="gray.100" justify="space-between" align="center" p="4">
-      <Box>
-      <Image
-                src="/logo.JPG"
-                alt="NextGenCoders Logo"
-                // className={styles.vercelLogo}
-                width={200}
-                height={60}
-                priority
-              />
-      
-             
+    <Flex bg="gray.100" justify="space-between" align="center" p="4" w="100%" as='nav' wrap='wrap'>
+      <Box pr={4}>
+        <Image
+          src="/logo.JPG"
+          alt="logo"
+          width={200}
+          height={60}
+        />
       </Box>
-      <Box>
-        <Input placeholder="Search..." />
+      <Box display="flex" alignItems="center">
+        <Input placeholder="Search..." size="lg" />
       </Box>
-      <Box>
-        <Button colorScheme="teal" variant="outline">
-          Write Post
-        </Button>
+      <Box  flex="0 0 auto" pl={[0,4]}>
+        <Button>Write Post</Button>
       </Box>
     </Flex>
+
 
   );
 };
