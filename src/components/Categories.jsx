3c4978e4b81d84507943183247b935d499469429
Yogita Negi <ynegi6@gmail.com>
2023-03-16 17:00:05 +0000

    Added category to categories section and links directing to each page

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import Link from "next/link";
 
 
 const Categories = () => {
   return (
-    <div>
-        <h1>Categories section</h1>
+    <div className="mx-auto w-1/4 lg:mx-0 lg:max-w-none lg:flex-none bg-blue-400 ">
+        <h1 className="text-lg font-bold mb-4 text-center">Categories section</h1>
+        <ul className="flex flex-col space-y-1 font-bold text-center">
+            <div>
+                <Link href="/categories/tutorials">Tutorials</Link>
+            </div>
+            <br/>
+            <div>
+                <Link href="/categories/news">News</Link>
+            </div>
+            <br/>
+            <div>
+                <Link href="/categories/libraries">Libraries</Link>
+            </div>
+            <br/>
+            <div>
+                <Link href="/categories/frameworks">Frameworks</Link>
+            </div>
+            <br/>
+        </ul>
     </div>
   )
 }
