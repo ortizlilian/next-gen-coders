efcabad56c42fe0a09a57054a32a0feea2550f92
Yogita Negi <ynegi6@gmail.com>
2023-03-16 20:37:47 +0000

    styling added to blog post so showing next to categories section

--- src/components/blogCard.js
+++ src/components/blogCard.js
@@ -7,7 +7,7 @@ function Card({ item }) {
 
     return (
         <Link href={`/${id}`}>
-          <div className="block my-4 max-w-lg rounded-lg bg-white p-6 shadow-lg dark:bg-neutral-700 ">
+          <div className="block my-4 max-w-lg rounded-lg bg-white p-6 shadow-lg dark:bg-neutral-700  ">
             <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">{title}</h5>
             <p className="mb-0 font-semibold text-neutral-500 dark:text-neutral-400">{author}</p>
             <p className="mb-6 font-light text-neutral-500 dark:text-neutral-300">{date}</p>
@@ -32,7 +32,7 @@ export default function BlogCards() {
 
     if(cardState.length > 0) {
         return(
-            <div className="flex flex-col">
+            <div className="flex flex-col w-1/2">
                 {cardState.map(item => {
                     return <Card key={item.id} item={item} />;
                 })}
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />        
       </Head>
       <main>      
-        <div>
+        <div className='flex gap-20'>
           <Categories />
           <BlogCards />
           
