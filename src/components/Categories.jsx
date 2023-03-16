1ed831ce54da1bbe3e5ea703f86d3765bc0e9094
Yogita Negi <ynegi6@gmail.com>
2023-03-16 17:59:42 +0000

    imported and added icons for news and librari category from material ui library

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,21 +1,25 @@
 import Link from "next/link";
+import FeedOutlinedIcon from '@mui/icons-material/FeedOutlined';
+import LibraryBooksOutlinedIcon from '@mui/icons-material/LibraryBooksOutlined';
 
 
 const Categories = () => {
   return (
-    <div className="mx-auto w-1/4 lg:mx-0 lg:max-w-none lg:flex-none bg-blue-400 ">
-        <h1 className="text-lg font-bold mb-4 text-center">Categories section</h1>
+    <div className="ml-10 mx-auto w-1/4 lg:mx-0 lg:max-w-none lg:flex-none bg-blue-400">
+        <h1 className="ml-10 text-lg font-bold mb-4 text-center">Categories section</h1>
         <ul className="flex flex-col space-y-1 font-bold text-center">
             <div>
                 <Link href="/categories/tutorials">Tutorials</Link>
             </div>
             <br/>
             <div>
-                <Link href="/categories/news">News</Link>
+                <Link href="/categories/news"><h2 className="flex items-center ml-20">
+                    <FeedOutlinedIcon className="w-6 h-6 "/>News</h2></Link>
             </div>
             <br/>
             <div>
-                <Link href="/categories/libraries">Libraries</Link>
+                <Link href="/categories/libraries"><h2 className="flex items-center space-x-6 ml-20">
+                    <LibraryBooksOutlinedIcon className="w-6 h-6"/>Libraries</h2></Link>
             </div>
             <br/>
             <div>
