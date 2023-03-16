5bb90a5e68bca9587fa2bb3c05ada6fa42185041
maplesyrupman <maplesyrupman@protonmail.com>
2023-03-16 15:19:34 -0400

    fixed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./pages/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./components/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{jsx,js}",
+    "./stc/**/**/*.{jsx,js}"
   ],
   theme: {
     extend: {},
