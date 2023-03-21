f745f481bf5beef69a15bdba4e9ff876eef3130d
Yogita Negi <ynegi6@gmail.com>
2023-03-21 19:17:15 +0000

    cards now showing on mid section

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }) {
   return(
     <div>
     <NavBar />
-    <Categories/>
+    {/* <Categories/> */}
     <Component {...pageProps} />
     </div> 
 
