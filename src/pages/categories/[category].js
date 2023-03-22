3878ac7c6d5fde4ade3860a6b5996a721fc6dc16
Lily Dell <lily.starleks@gmail.com>
2023-03-22 17:12:40 +0000

    updated page that lists posts by category

--- src/pages/categories/[category].js
+++ src/pages/categories/[category].js
@@ -1,41 +1,67 @@
-import { useRouter } from "next/router";
-import { getBlogPostsByCategory } from "@/utils/blogUtils";
-import blogData from '@/data/blogData.json'
+import React, { useState, useEffect } from "react";
+import { useRouter } from 'next/router'
+import Link from 'next/link';
+import axios from "axios";
 import Categories from "@/components/Categories";
 
-
-export default function CategoryPage({posts}) {
-    const router = useRouter();
-    const {category} = router.query;
+function Card({ item }) {
+    const { id, title, author, date, content } = item || {};
 
     return (
-        <div className='flex gap-20'>
-        <Categories/>
-        <div className="block my-4 max-w-lg rounded-lg bg-white p-6 shadow-lg dark:bg-neutral-700 w-4/5" >
-            <h1 className="mb-4 font-semibold text-primary dark:text-primary-500">Category: {category}</h1>
-            {posts.map((post) => (
-                <div key={post.id}>
-                    <h1 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">{post.title}</h1>
-                    <h2 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">{post.author}</h2>
-                    <p className="mb-6 font-light text-neutral-500 dark:text-neutral-300">{post.date}</p>
-                    <p className="mb-6 font-light text-neutral-500 dark:text-neutral-300">{post.content}</p>
-                </div>
-                
-            ))}
-        </div>
-        </div>
-       
-        
-    )
+        <Link href={`/blog-post/${title.split(' ').join('-')}`}>
+          <div className="block my-4 max-w-lg rounded-lg bg-white p-6 shadow-lg dark:bg-neutral-700 ">
+            <h1 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">{title}</h1>
+            <h2 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">{author}</h2>
+            <p className="mb-6 font-light text-neutral-500 dark:text-neutral-300">{date}</p>
+            <p className="mb-6 font-light text-neutral-500 dark:text-neutral-300">{content}</p>
+          </div>
+        </Link>
+    );
 }
 
-export async function getServerSideProps({params}) {
-    const {category} = params;
-    const posts = await getBlogPostsByCategory(category, blogData);
+export default function getCategoryPosts({posts}) {
+    const router = useRouter()
+    const {category} = router.query
 
+    const [categoryPostsState, setCategoryPostsState] = useState([]);
+
+    useEffect(() => {
+        axios
+        .get(`/api/blog/retrievecategory?category=${category}`)
+        .then(res => {
+            setCategoryPostsState(res.data);
+            console.log("test")
+        })
+    }, [category]);
+
+    if(categoryPostsState.length > 0) {
+        return(
+            <div className='flex gap-20'>
+                <Categories/>
+                <div className="block my-4 max-w-lg rounded-lg bg-white p-6 shadow-lg dark:bg-neutral-700 w-4/5" >
+                <h1 className="mb-4 font-semibold text-primary dark:text-primary-500">Category: {category}</h1>
+                    {categoryPostsState.map(item => {
+                        return <Card key={item.id} item={item} />;
+                    })}
+                </div>
+            </div>
+        );
+    } else {
+        return(
+            <div className='flex gap-20'>
+                <Categories/>
+                <div className="block my-4 max-w-lg rounded-lg bg-white p-6 shadow-lg dark:bg-neutral-700 w-4/5" >
+                <h1 className="mb-4 font-semibold text-primary dark:text-primary-500">Category: {category}</h1>
+                    No posts
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export async function getServerSideProps(context) {
     return {
-        props: {
-            posts
-        }
+        props: { },
     }
 }
